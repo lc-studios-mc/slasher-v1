6594c96b03bbcccb495c45e158cf5132d45da989
LuckedCoronet <lc45@lc-studios.net>
2026-03-27 16:22:48 +0900

    clean repo

--- src/bp/scripts/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * This is the main script, the entry point to our scripts.
- * We shouldn't write any business logic here.
- * So, we only import other modules that do the actual stuff.
- */
-
-import "./patches/player.js";
-import "./item_extender/internal.js";
-import "./slasher/slasher.js";
